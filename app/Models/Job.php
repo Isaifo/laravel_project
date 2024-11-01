7fd6d9bb2af178ffbbf5c69292ab5ba06fd38818
Isaifo <ysoca98@hotmail.com>
2024-11-01 09:41:07 -0300

    feat: add fetch from model table job_listings

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,42 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-{
-        return [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => 12000,
-        ],
-        [
-            'id' => 2,
-            'title' => 'PHP Developer',
-            'salary' => 1200,
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => 4000,
-        ]
-    ];
 
-}
-
-public static function find (int $id): array
-
-{
-   $job = Arr::first(static::all(), fn($job): bool => $job['id'] == $id);
-
-   if(!$job) {
-       abort(404);
-   }
-
-   return $job;
-}
-
+    protected $table = "job_listings";
 }
