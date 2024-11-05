af230d35b751cc243807b22a0d52370a7d9a3245
Isaifo <ysoca98@hotmail.com>
2024-11-05 20:18:15 -0300

    feat: relationship tags and jobs

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -19,4 +19,9 @@ class Job extends Model
     public function employer() {
         return $this->belongsTo(Employer::class);
     }
+
+    public function tags() {
+
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- database/migrations/2024_11_04_023148_create_tags_table.php
+++ database/migrations/2024_11_04_023148_create_tags_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(\App\Models\Tag::class)->constrained()->cascadeOnDelete();;
+            $table->foreignIdFor(\App\Models\Tag::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -31,6 +31,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
+       Schema::dropIfExists('tags');
+       Schema::dropIfExists('job_tag');
     }
 };
