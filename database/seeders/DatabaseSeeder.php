708209705d1ce649d10cd390f3bca15507eaa423
Isaifo <ysoca98@hotmail.com>
2024-11-09 13:59:15 -0300

    feat: database seedings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Mary',
+            'last_name' => 'Doe',
+            'email' => 'ysasa@hotmail.com',
         ]);
+
+     //   Job::factory()->create();
+        $this->call(JobSeeder::class);
+
+
     }
 }
