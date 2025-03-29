89d5f8a330543edf745ec95e420a2f2ebd1a596e
Isaifo <ysoca98@hotmail.com>
2025-03-29 11:45:50 -0300

    add a resource dedicated controller class

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,7 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::controller(JobController::class)->group(function () {
-    Route::view('/about', 'about');
+Route::resource('jobs', JobController::class);
 
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create',  'create');
-    Route::get('/jobs/{job}',  'show');
-    Route::post('/jobs',  'store');
-    Route::get('/jobs/{id}/edit',  'edit');
-    Route::patch('/jobs/{job}',  'update');
-    Route::delete('/jobs/{job}',  'destroy');
-});
-
-
-
-Route::view('/contac', 'contact');
+Route::view('/contact', 'contact');
 
