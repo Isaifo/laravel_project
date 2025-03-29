65db8349ad784c51eba34c11954672eb0beb7b3a
Isaifo <ysoca98@hotmail.com>
2025-03-29 11:30:28 -0300

    fead: add controller and views

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,8 +3,82 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate();
+
+
+        return view('jobs.index', ['jobs' => $jobs]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+
+    return view('jobs.show',['job' => $job]);
+    }
+
+    public function store(Job $job)
+    {
+        request()->validate([
+            'title' => 'required',
+            'salary' => 'required',
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]);
+
+        return redirect('/jobs');
+    }
+    public function edit(Job $job)
+    {
+
+
+        return view('jobs.edit',['job' => $job]);
+    }
+
+    public function update(Job $job)
+    {
+        request()->validate([
+            'title' => 'required',
+            'salary' => 'required',
+        ]);
+        // auth (On hold ...)
+        // update
+
+      //  $job = Job::findOrFail($id);
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+
+
+        // redirect
+
+        return redirect('/jobs/' . $job->id);
+    }
+
+
+    public function destroy(Job $job)
+    {
+        $job->delete();
+
+
+        return redirect('/jobs');
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,109 +1,20 @@
 <?php
 
-
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::get('/', function () {
-   return view('home');
-
-        $jobs = Job::all();
-      // dd($jobs[0]->title);
-
-});
-
-// index
-Route::get('/jobs', function () {
-
-   $jobs = Job::with('employer')->latest()->simplePaginate();
 
 
-    return view('jobs.index', ['jobs' => $jobs]);
+Route::view('/about', 'about');
 
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{id}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
-});
+Route::view('/contac', 'contact');
 
-// create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-
-});
-
-// show job
-Route::get('/jobs/{job}', function (Job $job) {
-
-  //$job = Job::find($id);
-
-
-    return view('jobs.show',['job' => $job]);
-});
-
-// Store
- Route::post('/jobs', function () {
-
-    request()->validate([
-        'title' => 'required',
-        'salary' => 'required',
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-
- });
-
-// Edit
- Route::get('/jobs/{id}/edit', function ($id) {
-
-    $job = Job::find($id);
-
-
-    return view('jobs.edit',['job' => $job]);
-});
-
-
-// Update
-
-Route::patch('/jobs/{job}', function (Job $job) {
-
-    request()->validate([
-        'title' => 'required',
-        'salary' => 'required',
-    ]);
-    // auth (On hold ...)
-    // update
-
-  //  $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-
-    // redirect
-
-    return redirect('/jobs/' . $job->id);
-});
-
-
-Route::delete('/jobs/{job}', function (Job $job) {
-
-    // auth
-    // delete the job
-
-     $job->delete();
-
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact',
-
-);
-});
