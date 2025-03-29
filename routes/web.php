b823552685d705a4c323e3dca07f47d12eaeda7c
Isaifo <ysoca98@hotmail.com>
2025-03-29 11:40:21 -0300

    modeling jobcontroller

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,19 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
+Route::controller(JobController::class)->group(function () {
+    Route::view('/about', 'about');
+
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create',  'create');
+    Route::get('/jobs/{job}',  'show');
+    Route::post('/jobs',  'store');
+    Route::get('/jobs/{id}/edit',  'edit');
+    Route::patch('/jobs/{job}',  'update');
+    Route::delete('/jobs/{job}',  'destroy');
+});
 
 
-Route::view('/about', 'about');
-
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{id}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::view('/contac', 'contact');
 
